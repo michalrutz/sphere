b43529bbff5696647de103c19bb777fbf2e5f370
Michal Rutz <michal.rutz@gmail.com>
2023-03-06 23:53:47 +0100

    fix glitch

--- gallery.js
+++ gallery.js
@@ -6,22 +6,23 @@ const tvAdvisor = {
     link: "https://tv-show-adviser-two.vercel.app/"
 };
 const memoGame = {
-    h: "the Memory Game",
+    h: "The Memory Game",
     tools: ["CSS","JavaScript","React"],
     p: "At the start, cards are placed face-down in neat rows and columns. A player then takes turns turning over two cards, one at a time, with the goal of finding matching pairs as quickly as possible. The application was created using React framework and demonstrates an advanced use of react hooks and JavaScript",
-    img: "memo_small.png",
+    img: "memo_small.jpg",
     link:"https://memo-game-six.vercel.app/"
 };
 const SimonGame = {
-    h: "the Simon Game",
+    h: "The Simon Game",
     tools: ["CSS","Vanilla JavaScript"],
     p: "The Simon is a classic memory game. The application creates a series of tones and lights and requires a player to repeat the sequence. If the user succeeds, the series becomes progressively longer and more complex. Once the user fails or the time limit runs out, the game is over.",
-    img: "simon.jpg",
+    img: "simon.JPG",
     link:"https://simon-game-red-eta.vercel.app/"
 };
 const projectsArray = [tvAdvisor, memoGame, SimonGame];
 
 
+//CARD 3D ANIMATION
 function animateCard(cardContainer, cardToAnimate, power) {
     let container = document.getElementById(cardContainer)
     let card = document.getElementById(cardToAnimate)
@@ -31,60 +32,68 @@ function animateCard(cardContainer, cardToAnimate, power) {
         let y = e.offsetY
         let w = container.offsetWidth
         let x = e.offsetX
-      
-        //console.log( ((h/2-y)/(h/2)).toFixed(2)) //gives reslts between -1/ +1
+
+
+     
+        console.log( ((h/2-y)/(h/2)).toFixed(2)) //gives reslts between -1/ +1
         let swingY = ((h/2-y)/(h/2)).toFixed(2)*power
         let swingX = ((w/2-x)/(w/2)).toFixed(2)*power
       
-        card.style.transform = `rotateX(${swingY}deg) rotateY(${-swingX}deg) scale(105%)`;
+        card.style.transform = `rotateX(${swingY}deg) rotateY(${-swingX}deg) scale(100%)`;
+        card.style.transition = "all 0.1s ease"
       });
       
       container.addEventListener('mouseenter', ()=>{
-          card.style.transition = "all 0.15s ease"
+          card.style.transition = "all 0.1s ease"
       }) 
       
       container.addEventListener('mouseleave', ()=>{
-          card.style.transform = `rotateX( ${0}deg) rotateY( ${0}deg) scale(100%)`;
+          card.style.transform = `rotateX( ${0}deg) rotateY( ${0}deg) scale(95%)`;
           card.style.transition = "all 1s ease"
       })
-}
+      container.addEventListener( "click", ()=>{
+        console.log("CLICK")
+        card.style.transform = `rotateX( ${0}deg) rotateY( ${0}deg) scale(95%)`;
+        card.style.transition = "all 1s ease"
+    } )
 
+}
+animateCard("container_about_cover", "about", 10)
+
+//GENEREATE GALLERY
 function gallery(data) {
     let gallery = []
-    let projects = document.getElementById("projects")
     for (let i = 0; i < data.length; i++) {
+        let projects = document.getElementById("projects")
         let article = projects.appendChild(document.createElement("article"));
-            article.classList.add("box")
+        article.classList.add("box")
+
             article.innerHTML = 
             `
-            <div class="container_wide hidden ${ i%2===0 ? "odd" : "" }" id="container_project_${i}" onclick="window.open('${data[i].link}')">
-                <div class="cover_wide"></div>
+            <div class="container_wide hidden ${i%2===0 ? "odd" : ""}" id="container_project_${i}" onclick="window.open('${data[i].link}')">
                 <div class="card_wide" id="project${i}">
                     <div class="description">
                         <h3>${data[i].h}</h3>
                         <div class="list">
-                            ${data[i].tools.map( t => `<tag>${t}</tag>` ).join("")}
+                            ${data[i].tools.map( (t) => `<tag>${t}</tag>` ).join("")}
                         </div>
                         <p>${data[i].p}</p>
                     </div>
                     <div class="picture" style="background:url(./static/${data[i].img}); background-size:contain" style="background-size:contain" ></div>
                 </div>
             </div>
-            <div class="break"></div>
+            <div class=break></div>
             `
-            animateCard( "container_project_"+i, "project"+i, 5 )
+            animateCard( "container_project_"+i, "project"+i, 4)
     }
     return gallery
 }
 gallery(projectsArray)
-//document.getElementById("projects").innerHTML = gallery(projectsArray).join("");
 
-animateCard("container_about_cover", "about", 10)
-animateCard("container_project_1", "project1", 2.5)
-
+//SCROLL ANIMATION
 const observer = new IntersectionObserver( (entries) => 
     entries.forEach( (entry) => {
-        console.log(entry)
+        //console.log(entry)
         if (entry.isIntersecting){
             entry.target.classList.add("visible")
         }
@@ -94,7 +103,5 @@ const observer = new IntersectionObserver( (entries) =>
     }
     )
 )
-
 const hiddenElements = document.querySelectorAll(".hidden");
-console.log(hiddenElements)
 hiddenElements.forEach( el => observer.observe(el) )
