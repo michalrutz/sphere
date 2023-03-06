ee3e55ab1819f22d16b5b0e30b31a79295c3a196
Michal Rutz <michal.rutz@gmail.com>
2023-03-06 14:36:02 +0100

    gallery generator

---
+++ gallery.js
@@ -0,0 +1,82 @@
+const tvAdvisor = {
+    h: "TV Show Advisor",
+    tools: ["CSS","JavaScript","React"],
+    p: "A TV show search engine with a sleek and minimalistic interface. Once a valid request is submitted, the application generates a comprehensive show page along with a set of recommendations. It utilizes modern function components and <strong>React hooks</strong> to dynamically create pages through <strong>third-party API</strong>. This project showcases my proficiency in handling <strong>asynchronous</strong> requests and databases.",
+    img: "tv_small.jpg",
+    link: "https://tv-show-adviser-two.vercel.app/"
+};
+const memoGame = {
+    h: "the Memory Game",
+    tools: ["CSS","JavaScript","React"],
+    p: "The cards are placed face down. In rows and columns. A player turns over two cards, one at a time. The object of the game is to collect all pairs as soon as possible <br>The application was created with help of react framework and demonstrates an advanced use of react hooks and JavaScript.",
+    img: "memo_small.png",
+    link:"https://memo-game-six.vercel.app/"
+};
+const SimonGame = {
+    h: "the Simon Game",
+    tools: ["CSS","Vanilla JavaScript"],
+    p: "The Simon is a classic memory game. The application creates a series of tones and lights and requires a player to repeat the sequence. If the user succeeds, the series becomes progressively longer and more complex. Once the user fails or the time limit runs out, the game is over.",
+    img: "simon.jpg",
+    link:"https://simon-game-red-eta.vercel.app/"
+};
+const projectsArray = [tvAdvisor, memoGame, SimonGame];
+
+
+function animateCard(cardContainer, cardToAnimate, power) {
+    let container = document.getElementById(cardContainer)
+    let card = document.getElementById(cardToAnimate)
+
+    container.addEventListener('mousemove', function(e) {
+        let h = container.offsetHeight
+        let y = e.offsetY
+        let w = container.offsetWidth
+        let x = e.offsetX
+      
+        //console.log( ((h/2-y)/(h/2)).toFixed(2)) //gives reslts between -1/ +1
+        let swingY = ((h/2-y)/(h/2)).toFixed(2)*power
+        let swingX = ((w/2-x)/(w/2)).toFixed(2)*power
+      
+        card.style.transform = `rotateX(${swingY}deg) rotateY(${-swingX}deg) scale(100%)`;
+      });
+      
+      container.addEventListener('mouseenter', ()=>{
+          card.style.transition = "all 0.15s ease"
+      }) 
+      
+      container.addEventListener('mouseleave', ()=>{
+          card.style.transform = `rotateX( ${0}deg) rotateY( ${0}deg) scale(80%)`;
+          card.style.transition = "all 1s ease"
+      })
+}
+
+function gallery(data) {
+    let gallery = []
+    let projects = document.getElementById("projects")
+    for (let i = 0; i < data.length; i++) {
+        let article = projects.appendChild(document.createElement("article"));
+            article.classList.add("box")
+            article.innerHTML = 
+            `
+            <div class="container_wide" id="container_project_${i}" onclick="window.open('${data[i].link}')">
+                <div class="cover_wide"></div>
+                <div class="card_wide" id="project${i}">
+                    <div class="description">
+                        <h3>${data[i].h}</h3>
+                        <div class="list">
+                            ${data[i].tools.map( t => `<tag>${t}</tag>` ).join("")}
+                        </div>
+                        <p>${data[i].p}</p>
+                    </div>
+                    <div class="picture" style="background:url(public/static/${data[i].img}); background-size:contain" style="background-size:contain" ></div>
+                </div>
+            </div>
+            `
+            animateCard( "container_project_"+i, "project"+i, 5 )
+    }
+    return gallery
+}
+gallery(projectsArray)
+//document.getElementById("projects").innerHTML = gallery(projectsArray).join("");
+
+animateCard("container_about_cover", "about", 10)
+animateCard("container_project_1", "project1", 2.5)
--- main.js
+++ main.js
@@ -203,32 +203,6 @@ window.smoothScroll = function(target) {
   
 */
 
-let containter = document.getElementById("container_cover");
-let card = document.getElementById("about");
-
-containter.addEventListener('mousemove', function(e) {
-  let h = containter.offsetHeight
-  let y = e.offsetY
-  let w = containter.offsetWidth
-  let x = e.offsetX
-
-  //console.log( ((h/2-y)/(h/2)).toFixed(2)) //gives reslts between -1/ +1
-  let swingY = ((h/2-y)/(h/2)).toFixed(2)*10
-  let swingX = ((w/2-x)/(w/2)).toFixed(2)*10
-
-  card.style.transform = `rotateX(${swingY}deg) rotateY(${-swingX}deg) translateZ(50px)`;
-});
-
-
-containter.addEventListener('mouseenter', ()=>{
-    card.style.transition = "all 0.1s ease"
-}) 
-
-containter.addEventListener('mouseleave', ()=>{
-    card.style.transform = `rotateX( ${0}deg) rotateY( ${0}deg)`;
-    card.style.transition = "all 1s ease"
-})
-
 document.onscroll = (e) => {
     console.log("e")
 
