ec74b00baca2b784d9711566bf3be5a9deffe5de
Michal Rutz <michal.rutz@gmail.com>
2023-05-30 16:39:20 +0200

    memo-game update

--- animations.js
+++ animations.js
@@ -1,33 +1,9 @@
-const tvAdvisor = {
-    h: "TV Show Advisor",
-    tools: ["CSS","JavaScript","React"],
-    p: "A TV show search engine with a sleek and minimalistic interface. Once a valid request is submitted, the application generates a comprehensive show page along with a set of recommendations. It utilizes modern function components and <strong>React hooks</strong> to dynamically create pages through <strong>third-party API</strong>. This project showcases my proficiency in handling <strong>asynchronous</strong> requests and databases.",
-    img: "tv_small.jpg",
-    link: "https://tv-show-adviser-two.vercel.app/"
-};
-const memoGame = {
-    h: "The Memory Game",
-    tools: ["CSS","JavaScript","React"],
-    p: "At the start, cards are placed face-down in neat rows and columns. A player then takes turns turning over two cards, one at a time, with the goal of finding matching pairs as quickly as possible. The application was created using React framework and demonstrates an advanced use of react hooks and JavaScript",
-    img: "memo_small.jpg",
-    link:"https://memo-game-six.vercel.app/"
-};
-const SimonGame = {
-    h: "The Simon Game",
-    tools: ["CSS","Vanilla JavaScript"],
-    p: "The Simon is a classic memory game. The application creates a series of tones and lights and requires a player to repeat the sequence. If the user succeeds, the series becomes progressively longer and more complex. Once the user fails or the time limit runs out, the game is over.",
-    img: "simon.JPG",
-    link:"https://simon-game-red-eta.vercel.app/"
-};
-const TShirtShop = {
-    h: "3D T-Shirt Cusomizer",
-    tools: ["CSS","JavaScript","React", "WebGl", "Three.js"],
-    p: "  Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong> and define your own style.",
-    img: "tshirt_small.jpg",
-    link:"https://ur-t-shirt.vercel.app/"
-};
+import { list } from "./assets/project.js"
 
-const projectsArray = [TShirtShop, tvAdvisor, memoGame, SimonGame];
+
+
+
+const projectsArray = [...list];
 
 
 //CARD 3D ANIMATION
